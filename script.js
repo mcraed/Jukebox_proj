1c4ce7b5edfd612c04049481b1e8f0a7de55c0ad
Donovan McRae <d_mcrae@ymail.com>
2016-05-18 17:20:25 -0400

    adding functionality to 'last' button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+
 	console.log('working');
 
 	function Jukebox(){
@@ -22,31 +24,42 @@ $(document).ready(function(){
 		var track_count = songs.length;
 		var i = Math.floor((Math.random() * track_count));
 		var clicked = true;	// regarding play click event
+		 now_playing = songs[i];
+		 play_history = [];
+		var	h	= play_history.length - 1;
+		var back_clicked = false
 
 		audio.src = songs[i].file_name;
 
-		this.play = function(){
-			audio.play();
-		}
+		var history_push = function(){
+			play_history.push(now_playing);
+		};
 
 		var random_track = function(){
 			audio.pause();
 			audio.currentTime = 0;
 			i = Math.floor((Math.random() * track_count));
-			audio.src = playlist[i];
+			audio.src = songs[i].file_name;
 			audio.play();
+			now_playing = songs[i];
+			$('#now-playing').text(now_playing.name + ' - ' + now_playing.artist);
+			history_push(now_playing);
 		};
 
 		var show_shuffle_status = function(){
 			if(shuffle){
 				$('#shuffle-status').text('On');
 			}else{
-				$('#shuffle-status').text('On');
+				$('#shuffle-status').text('Off');
 			};
 		};
-
 		show_shuffle_status();
 
+		this.play = function(){
+			audio.play();
+			$('#now-playing').text(now_playing.name + ' - ' + now_playing.artist);
+			history_push(now_playing);
+		}
 		
 		$('#aud-play').on('click', function(){
 
@@ -59,11 +72,6 @@ $(document).ready(function(){
 			};
 		});
 
-		$('#aud-stop').click(function(){
-			audio.pause();
-			audio.currentTime  = 0;	
-		});
-
 		$('#aud-shuffle').click(function(){
 
 			if(!shuffle){
@@ -72,12 +80,13 @@ $(document).ready(function(){
 			}else{
 				shuffle =  false;
 			};
-			$('#shuffle-status').text(shuffle);
+
+			show_shuffle_status();
 		});
 
 		$('#aud-next').on( 'click', function(){
 		
-			if(shuffle == true){
+			if(shuffle){
 				random_track();
 			}else{
 
@@ -86,8 +95,34 @@ $(document).ready(function(){
 				}else{
 					i = i + 1;
 				};
-				audio.src = playlist[i];
+				audio.src = songs[i].file_name;
 				audio.play();
+				now_playing = songs[i];
+				$('#now-playing').text(now_playing.name + ' - ' + now_playing.artist);
+				history_push(now_playing);
+			};
+		});
+
+		$('#aud-last').on('click', function(){
+
+			if(!back_clicked){
+				audio.currentTime = 0;
+				audio.play();
+				back_clicked = true;
+				h = play_history.length - 1;
+				console.log('you did not click this before, but now we can see that you have! Heres the song again');
+			}else{
+					if(h == 0){
+						console.log("You're right where you started!");
+						h = 0;
+					}else{
+						h = h - 1
+						audio.src = play_history[h].file_name;
+						audio.play();
+						now_playing = play_history[h];
+						$('#now-playing').text(now_playing.name + ' - ' + now_playing.artist);
+						console.log('you already clicked this. Were digging in your history to grab the song you heard before this one');
+				};
 			};
 		});
 
