9f250e3c09b281d08d9dcfa0b5f2530a095ab4df
Donovan McRae <d_mcrae@ymail.com>
2016-05-19 13:10:31 -0400

    adding jquery/ attempting to allow users to add audio files to playlist

--- script.js
+++ script.js
@@ -12,12 +12,13 @@ $(document).ready(function(){
 			this.file_name = file_name;
 		}
 		// default songs
-		var american_idiot = new Song('American Idiot', 'Green Day', 'audio/American_idiot.mp3'); 
 		var castles = new Song('Castles Made of Sand', 'Jimi Hendrix', 'audio/Castles_Made_of_Sand.mp3'); 
 		var cemetery_drive = new Song('Cemetery Drive', 'My Chemical Romance', 'audio/Cemetery_drive.mp3'); 
 		var helena = new Song('Helena', 'My Chemical Romance', 'audio/Helena.mp3');
+		var berry = new Song('The Blacker the Berry', 'Kendrick Lamar', 'audio/The_Blacker_the_Berry.mp3')
+		var taylor_gang = new Song('Taylor Gang', 'Wiz Khalifa', 'audio/Taylor_Gang.mp3')
 
-		var songs = [american_idiot, castles, cemetery_drive, helena];
+		 songs = [taylor_gang, berry, castles, cemetery_drive, helena];
 
 		var audio = document.createElement('audio');
 		var shuffle = true; // default shuffle setting
@@ -27,9 +28,20 @@ $(document).ready(function(){
 		 now_playing = songs[i];
 		 play_history = [];
 		var	h	= play_history.length - 1;
-		var back_clicked = false
 
 		audio.src = songs[i].file_name;
+
+		// trying to add functionality so users to add song files to playlist
+		$('#file-name-input').on('click', function(){
+			var song;
+			var name = $('#song-name-input').val();
+			var artist = $('#artist-name-input').val();
+			var file_name = $('#file-name-input').val();
+
+			song = new Song(name, artist, file_name);
+			songs.push(song);
+			console.log(name +' by ' + artist + " has been added to your playlist!");
+		});
 
 		var history_push = function(){
 			play_history.push(now_playing);
@@ -55,6 +67,19 @@ $(document).ready(function(){
 		};
 		show_shuffle_status();
 
+
+		$(audio).on('ended', function(){
+			if(shuffle){
+				random_track();				
+			}else{
+				i = i + 1
+				audio.src = songs[i].file_name;
+				audio.play();
+				now_playing = songs[i];
+				history_push(now_playing);
+			}
+		});
+
 		this.play = function(){
 			audio.play();
 			$('#now-playing').text(now_playing.name + ' - ' + now_playing.artist);
@@ -72,6 +97,11 @@ $(document).ready(function(){
 			};
 		});
 
+		$('aud-stop').on('click', function(){
+			audio.pause();
+			audio.currentTime = 0;
+		});
+
 		$('#aud-shuffle').click(function(){
 
 			if(!shuffle){
@@ -86,23 +116,20 @@ $(document).ready(function(){
 
 		$('#aud-last').on('click', function(){
 
-			if(!back_clicked){
+			if(audio.currentTime > 4){
 				audio.currentTime = 0;
-				audio.play();
-				back_clicked = true;
+				audio.play();	
 				h = play_history.length - 1;
-				console.log('you did not click this before, but now we can see that you have! Heres the song again');
 			}else{
-					if(h == 0){
-						console.log("You're right where you started!");
+					if(h == 0){					
 						h = 0;
+						// maybe alert the user that it is the start of their play history
 					}else{
 						h = h - 1
 						audio.src = play_history[h].file_name;
 						audio.play();
 						now_playing = play_history[h];
 						$('#now-playing').text(now_playing.name + ' - ' + now_playing.artist);
-						console.log('you already clicked this. Were digging in your history to grab the song you heard before this one');
 				};
 			};
 		});
@@ -111,17 +138,14 @@ $(document).ready(function(){
 		
 		// testing if element is NOT in an array
 		if(now_playing === play_history[h] ){
-			
-			if(h === play_history.length - 1){
+			if(h == play_history.length - 1){
 				random_track();
-				console.log('well you thats all of your history! heres a random song; lets go from there')
 			}else{
 				h = h + 1;
 				audio.src = play_history[h].file_name;
 				audio.play();
 				now_playing = play_history[h];
 				$('#now-playing').text(now_playing.name + ' - ' + now_playing.artist);
-				console.log('this song is coming from your history')
 			};
 		}else{
 			if(shuffle){
